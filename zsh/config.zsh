644a0f12792a87baa2fd755c783824e022af8741
ac2393921 <ac2393921@gmail.com>
2021-12-28 01:10:07 +0900

    update: .gitignoreの修正とignoreされていたファイルをadd

---
+++ zsh/config.zsh
@@ -0,0 +1,31 @@
+#################################  HISTORY  #################################
+# history
+HISTFILE=$HOME/.zsh-history # 履歴を保存するファイル
+HISTSIZE=100000             # メモリ上に保存する履歴のサイズ
+SAVEHIST=1000000            # 上述のファイルに保存する履歴のサイズ
+
+# share .zshhistory
+setopt inc_append_history   # 実行時に履歴をファイルにに追加していく
+setopt share_history        # 履歴を他のシェルとリアルタイム共有する
+
+#################################  COMPLEMENT  #################################
+# enable completion
+autoload -Uz compinit && compinit
+
+# 補完候補をそのまま探す -> 小文字を大文字に変えて探す -> 大文字を小文字に変えて探す
+zstyle ':completion:*' matcher-list '' 'm:{[:lower:]}={[:upper:]}' '+m:{[:upper:]}={[:lower:]}'
+
+### 補完方法毎にグループ化する。
+zstyle ':completion:*' format '%B%F{blue}%d%f%b'
+zstyle ':completion:*' group-name ''
+
+
+### 補完侯補をメニューから選択する。
+### select=2: 補完候補を一覧から選択する。補完候補が2つ以上なければすぐに補完する。
+zstyle ':completion:*:default' menu select=2
+#################################  OTHERS  #################################
+# automatically change directory when dir name is typed
+setopt auto_cd
+
+# disable ctrl+s, ctrl+q
+setopt no_flow_control
