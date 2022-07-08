8795b9d76946411ab3ac6974087ec2b12b20ea11
ac2393921 <ac2393921@gmail.com>
2022-07-09 01:53:04 +0900

    add: bundle dir & file

---
+++ .bundle/install.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+###[ Homebrew ]#################################################################
+brew upgrade
+brew bundle --file ${BUNDLE_DIR}/Brewfile
+brew services start yabai
+
+###[ Zsh ]######################################################################
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
+source ${HOME}/.zshrc
+
+###[ Rust ]#####################################################################
+curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
+source ${HOME}/.cargo/env
+rustup --version
+rustc --version
+
+###[ Cargo ]####################################################################
+cargo --version
+cargo install $(cat ${BUNDLE_DIR}/Cargofile)
+
+###[ Vscode ]###################################################################
+for vsc in $(cat ${BUNDLE_DIR}/Vscfile); do
+    code-insiders --install-extension $vsc
+done
