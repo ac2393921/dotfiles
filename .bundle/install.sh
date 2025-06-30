4dfb3d4f9ed8396a4d623aa51f10ef47e298f624
ac2393921 <ac2393921@gmail.com>
2025-06-30 21:18:28 +0900

    feat: bundle

--- .bundle/install.sh
+++ .bundle/install.sh
@@ -1,25 +1,25 @@
-#! /bin/bash
+# #! /bin/bash
 
-###[ Homebrew ]#################################################################
+# ###[ Homebrew ]#################################################################
 brew upgrade
 brew bundle --file ${BUNDLE_DIR}/Brewfile
 brew services start yabai
 
-###[ Zsh ]######################################################################
+# ###[ Zsh ]######################################################################
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
 source ${HOME}/.zshrc
 
-###[ Rust ]#####################################################################
+# ###[ Rust ]#####################################################################
 curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
 source ${HOME}/.cargo/env
 rustup --version
 rustc --version
 
-###[ Cargo ]####################################################################
+# ###[ Cargo ]####################################################################
 cargo --version
 cargo install $(cat ${BUNDLE_DIR}/Cargofile)
 
 ###[ Vscode ]###################################################################
 for vsc in $(cat ${BUNDLE_DIR}/Vscfile); do
-    code-insiders --install-extension $vsc
+    code --install-extension $vsc
 done
